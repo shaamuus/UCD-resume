a7eaf049257f8467da285ab14c7b51af614f8e7f
Shamus Birch <th35h4y@gmail.com>
2020-07-27 21:53:41 +0000

    minor changes

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,15 @@
-console.log("Hello")
+function sendMail(contactForm) {
+    emailjs.send("gmail", "shamus", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.name.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        });
+        return false;  // To block from loading a new page
+}
